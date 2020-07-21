2578a600935a10d48d0f17042432a6fa29540cf9
ごとうあつし <aagoto999@gmail.com>
2020-07-21 13:26:50 +0900

    photo change

--- app.js
+++ app.js
@@ -14,5 +14,25 @@ $(function(){
         $('.js-toggle-sp-menu').toggleClass('active');
         $('.js-toggle-sp-menu-target').toggleClass('active');
     });
+
+    let mySwiper = new Swiper ('#swiper', {
+        autoplay: {
+          delay: 6000,
+        },
+        
+        speed:1000,
+        
+        loop: true,
+        
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
     
 });
